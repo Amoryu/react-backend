1dcec469e72acc1c619c22ecb840a8318e1dd870
庄嘉华 <10921549+zhuang-jiahua@user.noreply.gitee.com>
2023-08-15 22:53:50 +0800

    编写首页基本布局

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ export default function App() {
   const outlet = useRoutes(router)
   return (
     <>
-      <Link to="/home">Home</Link>
+      {/* <Link to="/home">Home</Link>
       <Link to="/about">About</Link>
-      <Link to="/user">User</Link>
+      <Link to="/user">User</Link> */}
       
 
       {/* 占位符组件，类似router-view 

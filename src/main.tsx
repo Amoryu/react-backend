413620530741837a0b07cbed715a2eb0dad85743
庄嘉华 <10921549+zhuang-jiahua@user.noreply.gitee.com>
2023-08-15 21:02:35 +0800

    初始化样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//初始化样式
+import 'reset-css'
+
+// 全局样式
+import './assets/styles/global.scss'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
